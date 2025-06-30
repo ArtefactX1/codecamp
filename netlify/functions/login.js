25228232e30b7cbb7e9e0042d21f3860d5e2e004
ArtefactX1 <123149085+ArtefactX1@users.noreply.github.com>
2025-06-30 11:06:15 +0700

    Update login.js

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -1,3 +1,4 @@
+const { createClient } = require('@supabase/supabase-js');
 exports.handler = async function (event) {
     if (event.httpMethod !== 'POST') return { statusCode: 405, body: 'Method Not Allowed' };
     const { email, password } = JSON.parse(event.body);
