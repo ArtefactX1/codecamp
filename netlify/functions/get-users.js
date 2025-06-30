d7e5eddb2be0902c2fbc1228029eef22658d7a77
ArtefactX1 <123149085+ArtefactX1@users.noreply.github.com>
2025-06-30 10:59:03 +0700

    Create get-users.js

---
+++ netlify/functions/get-users.js
@@ -0,0 +1,8 @@
+exports.handler = async function() {
+    const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+    try {
+        let { data, error } = await supabase.from('users').select('name, email, role');
+        if (error) throw error;
+        return { statusCode: 200, body: JSON.stringify(data) };
+    } catch (error) { return { statusCode: 500, body: JSON.stringify({ message: error.message }) }; }
+};
